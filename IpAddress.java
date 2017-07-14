7fcd4636eae9ecf00114b7f362ed5a909f6c5c18
dineshadam <dineshadam648@gmail.com>
2017-07-14 15:08:42 +0530

    Add files via upload

--- IpAddress.java
+++ IpAddress.java
@@ -1,31 +1,33 @@
 package tech;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class IpAddress {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner read=new Scanner(System.in);
-		System.out.println("Enter the 11 digit number");
-		String b=read.next();
+		System.out.println("Enter the Ip in 11 digit number");
+		String ip=read.next();
 		try {
-		long a=Integer.parseInt(b);
+			if(ip.length()==11) {
+		int num=Integer.parseInt(ip.substring(0, 6));
+		int num1=Integer.parseInt(ip.substring(5, 11));
+		if(num>=0&&num1>=0)
+		{ 
 		
-		String ip=String.valueOf(a);
-
-		if(ip.length()==11) {
-				
 			System.out.println(ip.substring(0,3)+"."+ip.substring(3,6)+"."+ip.substring(6,9)+"."+ip.substring(9,11));
 			System.out.println("Ip address is valid");
 		}
-		else
-			System.out.println("ip address is invalid");
-
-		}
-		catch(Exception e) {
-				System.out.println("Enter the string in number");
+			}
+		else {
+			System.out.println("ip address length is less");
 		}
 		}
 		
+		catch(Exception e) {
+			System.out.println("Enter the ip in number");
+		}
+	}
 
 }
