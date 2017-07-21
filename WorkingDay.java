e482ceb1828c2d5b00255bd6906b177e932e5f0a
dineshadam <dineshadam648@gmail.com>
2017-07-21 15:45:24 +0530

    Add files via upload

---
+++ WorkingDay.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.util.Scanner;
+
+public class WorkingDay {
+
+	public static void main(String[] args) {
+		Scanner read=new Scanner(System.in);
+		System.out.println("Enter the day");
+		String str=read.next();
+		if(str.equalsIgnoreCase("mon")||str.equalsIgnoreCase("tue")||str.equalsIgnoreCase("wed")||str.equalsIgnoreCase("thus")||str.equalsIgnoreCase("fri")||str.equalsIgnoreCase("monday")||str.equalsIgnoreCase("tuesday")||str.equalsIgnoreCase("wednesday")||str.equalsIgnoreCase("thursday")||str.equalsIgnoreCase("friday"))
+		{
+			System.out.println("true");
+		}
+		else if(str.equalsIgnoreCase("sat")||str.equalsIgnoreCase("sun")||str.equalsIgnoreCase("sunday")||str.equalsIgnoreCase("saturday"))
+		{
+			System.out.println("false");
+
+		}
+		else {
+			System.out.println("The day is not valid");
+		}
+			
+	}
+
+}
