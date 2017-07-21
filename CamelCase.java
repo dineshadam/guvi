e1109a372205a7cdf2540e2cb01a0ed009a62a5e
dineshadam <dineshadam648@gmail.com>
2017-07-21 15:06:45 +0530

    Add files via upload

---
+++ CamelCase.java
@@ -0,0 +1,19 @@
+package string;
+
+import java.util.Scanner;
+
+public class CamelCase {
+	public static void main(String[] args) {
+		Scanner read=new Scanner(System.in);
+		System.out.println("enter the String: ");
+
+		String str=read.nextLine();
+		//StringBuffer sb=new StringBuffer(str);
+		String []str1=str.split(" ");
+		for(String k:str1){
+			System.out.print(Character.toUpperCase(k.charAt(0))+(k.substring(1,k.length())));
+		}
+		
+	}
+
+}
